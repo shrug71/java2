71ea12a855c2e165a57035af5043851e1d7d29e5
shrug71 <74814085+shrug71@users.noreply.github.com>
2021-03-03 16:58:15 +0530

    Update Checkbox.java

--- Checkbox.java
+++ Checkbox.java
@@ -1,41 +1,20 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.border.EmptyBorder;
-
-
-
-public class CheckboxEx1
-{
-JFrame jf;
-Checkbox chk1, chk2, chk3, chk4;
-
-CheckboxEx1()
-{
-jf= new JFrame("Variants of Checkbox");
-
-//Creating an no-message Checkbox
-chk1 = new Checkbox();		
-
-//Creating a pre-selected Checkbox with a message 
-chk2 = new Checkbox("Yes",true);
-
-//Creating an unselected Checkbox with a message	
-chk3 = new Checkbox("No");	
-		
-
-jf.add(chk1);
-jf.add(chk2);
-jf.add(chk3);
-
-jf.setLayout(new FlowLayout());
-jf.setSize(300,120);
-jf.setVisible(true);
-}
-
-public static void main(String... ar)
-{
-new CheckboxEx1();
-}
-
-}
+import java.awt.*;  
+public class CheckboxDemo1  
+{  
+  CheckboxDemo1(){
+    Frame checkB_f= new Frame("studytonight ==>Checkbox Example");  
+    Checkbox ckbox1 = new Checkbox("Yes", true);  
+    ckbox1.setBounds(100,100, 60,60);  
+    Checkbox ckbox2 = new Checkbox("No");  
+    ckbox2.setBounds(100,150, 60,60);  
+    checkB_f.add(ckbox1);  
+    checkB_f.add(ckbox2);  
+    checkB_f.setSize(400,400);  
+    checkB_f.setLayout(null);  
+    checkB_f.setVisible(true);  
+  }  
+  public static void main(String args[])  
+  {  
+    new CheckboxDemo1();  
+  }  
+}   
